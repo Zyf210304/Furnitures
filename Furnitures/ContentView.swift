f3c72348d7e20a9083412c592ca722d93d8bf328
张亚飞 <672233580@qq.com>
2021-01-25 13:20:16 +0800

    Initial Commit

---
+++ Furnitures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Furnitures
+//
+//  Created by 亚飞 on 2021/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
